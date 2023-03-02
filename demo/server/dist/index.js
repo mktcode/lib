8f4f5dd1cd6037a5549f4fba12540f78192651fb
mktcode <kontakt@markus-kottlaender.de>
2023-03-02 18:59:07 +0100

    updated demo

--- demo/server/dist/index.js
+++ demo/server/dist/index.js
@@ -3,6 +3,8 @@
 // src/index.ts
 var import_good_first_web3_issues = require("@mktcodelib/good-first-web3-issues");
 var gfw3i = new import_good_first_web3_issues.GoodFirstWeb3Issues({
-  githubToken: process.env.PAT || ""
+  githubToken: process.env.PAT || "",
+  debug: true,
+  snycInterval: 1e3 * 10
 });
 gfw3i.run();
--- demo/server/dist/index.mjs
+++ demo/server/dist/index.mjs
@@ -1,6 +1,8 @@
 // src/index.ts
 import { GoodFirstWeb3Issues } from "@mktcodelib/good-first-web3-issues";
 var gfw3i = new GoodFirstWeb3Issues({
-  githubToken: process.env.PAT || ""
+  githubToken: process.env.PAT || "",
+  debug: true,
+  snycInterval: 1e3 * 10
 });
 gfw3i.run();
