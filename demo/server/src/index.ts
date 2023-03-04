6d4c0e594ed30553e61c5b77a3675a8f9edf767f
mktcode <kontakt@markus-kottlaender.de>
2023-03-04 15:27:29 +0100

    use scientific notation

--- demo/server/src/index.ts
+++ demo/server/src/index.ts
@@ -3,7 +3,7 @@ import { GoodFirstWeb3Issues } from '@mktcodelib/good-first-web3-issues';
 const gfw3i = new GoodFirstWeb3Issues({
   githubToken: process.env.PAT || '',
   debug: true,
-  syncInterval: 1000 * 10,
+  syncInterval: 1e3 * 10,
 })
 
 gfw3i.run()
