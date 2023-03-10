bb2718c8860aafcfc89c83054a83c1614b195823
mktcode <kontakt@markus-kottlaender.de>
2023-03-10 14:26:02 +0100

    updated demo

--- demo/server/src/good-first-web3-issues.ts
+++ demo/server/src/good-first-web3-issues.ts
@@ -3,7 +3,7 @@ import { GoodFirstWeb3Issues } from '@mktcodelib/good-first-web3-issues';
 const gfw3i = new GoodFirstWeb3Issues({
   githubToken: process.env.PAT || '',
   debug: true,
-  syncInterval: 1e3 * 60,
+  rateLimit: 2500,
   port: 3009,
   corsOrigin: /localhost:3000/,
 })
